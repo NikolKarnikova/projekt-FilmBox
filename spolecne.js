35be353a5c85cf1831ae48f7dd86a67ce8ac3a5f
Nikol Karnikova <ni.karnikova@gmail.com>
2024-04-11 18:33:27 +0200

    pridana zmena ikony pri rozbaleni menu

--- spolecne.js
+++ spolecne.js
@@ -1,10 +1,17 @@
 const addshow = () => {
     const polozkyMenu = document.querySelector('#menu-polozky');
+    const tlacitkoMenu = document.querySelector('#menu-tlacitko');
+
     if (polozkyMenu.classList.contains('show')) {
         polozkyMenu.classList.remove('show');
+        tlacitkoMenu.innerHTML = '<i class="fas fa-bars"></i>';
+
     } else {
         polozkyMenu.classList.add('show');
+        tlacitkoMenu.innerHTML = '<i class="fas fa-times"></i>';
+
     }
 };
 
 document.querySelector('#menu-tlacitko').addEventListener('click',addshow);
+
